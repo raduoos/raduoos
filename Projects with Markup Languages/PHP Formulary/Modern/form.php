99165e7ffc36433f470bb5c36eb0e0b774da3aec
Radostin Ivanov <rvaleriev042@gmail.com>
2023-11-19 12:49:36 +0100

    19.11.23 Update

---
+++ Projects with Markup Languages/PHP Formulary/Modern/form.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Forms - The answers:</title>
+	</head>
+
+	<body>
+
+		<?php
+			$email_message = "Details of the formulary of contact:<br> <br>";
+			$email_message .= "Name: " . $_POST['name'] . "<br>";
+			$email_message .= "E-Mail Address: " . $_POST['email'] . "<br>";
+			$email_message .= "Comments: " . $_POST['comments'] . "<br>";
+			$email_message .= "Favorite Programming Language: " . $_POST['favoriteLanguage'] . "<br>";
+			$email_message .= "Vehicle brand: " . $_POST['cars'] . "<br>";
+			$email_message .= "Join newsletter: " . $_POST['email_notifications'] . "<br>";
+			echo $email_message;
+		?>
+
+	</body>
+</html>
